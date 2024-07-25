743b33d702062167324a31d6a5b14d1b61e60b1a
Krunal Patel <patel.krunal.tech@gmail.com>
2024-07-25 12:14:02 +0000

    feat: made some changes

---
+++ web/app.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def mainmenu():
+
+    return """
+    <html>
+    <body>
+    <center><h1>Hello World! from AWS CodeDeploy.</h1><br/>
+	<h2>I am Iron-Man.</h2>
+    </body>
+    </html>"""
+
+if __name__ == "__main__":
+	app.run(debug=False,host='0.0.0.0', port=80)
